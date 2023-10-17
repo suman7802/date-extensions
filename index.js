fb405c0819b5677b3a491e00fb3e94bf49a54a14
suman sharma <sumans@sumans-Air.domain.name>
2023-10-17 22:43:56 +0545

    cors

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', async () => {
   const resultDiv = document.getElementById('result');
 
   try {
-    const response = await fetch(
-      'https://jsonplaceholder.typicode.com/posts/1'
-    );
+    const response = await fetch('https://nepalidate.onrender.com/api/nepali', {
+      mode: 'no-cors',
+    });
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -22,3 +22,4 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 // <pre>${JSON.stringify(data, null, 2)}</pre>
 // https://nepalidate.onrender.com/api/nepali
+// 'https://jsonplaceholder.typicode.com/posts/1'
