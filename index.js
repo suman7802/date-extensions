980f776b458798e2b476dce28231225ec8c683b2
suman sharma <sumans@sumans-Air.domain.name>
2023-10-17 22:10:37 +0545

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  const resultDiv = document.getElementById('result');
+
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/posts/1'
+    );
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    resultDiv.innerHTML = `
+          <pre>${JSON.stringify(data)}</pre>
+      `;
+  } catch (error) {
+    resultDiv.innerHTML = `
+          <h2>Error</h2>
+          <p>${error.message}</p>
+      `;
+  }
+});
+
+// <pre>${JSON.stringify(data, null, 2)}</pre>
+// https://nepalidate.onrender.com/api/nepali
